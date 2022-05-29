44e05be7072a0cf65815f1ad225554d4c3d7c93d
AndreHammel <hammel.engineer@gmail.com>
2022-05-29 13:28:39 -0300

    quando clicado o contador continua a fazer o incremento ou decremento

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,26 +1,39 @@
-var currentNumberWrapper = document.getElementById('currentNumber')
-var currentNumber = 0;
-var timer = console.time();
+let currentNumber = document.getElementById('currentNumber')
 
-function increment() {
-    while (timer<10s) {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-    
-    if (currentNumber > 0) {
-        currentNumberWrapper.style.color = 'lightgreen';
-    } else if (currentNumber == 0){
-        currentNumberWrapper.style.color = 'white';
-    }
-}
+// operation é a função que realmente faz as contas, recebe o typeButton como argumento
+// isso evita ficar repetindo código, assim evitar ter uma função para incrementar
+// outra pra decrementar etc.
+// Invés de usar um if para fazer a condicional se o botão é 'add' ou 'sub', foi feito
+// o que é chamado de ternário, se o botão é do tipo  'add' então startValue++
+// caso não seja faça startValue--
+function operation(typeButton) {
+  startValue = currentNumber.innerText
+  typeButton === 'add' ? startValue++ : startValue--
+  currentNumber.innerText = startValue
 }
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-    if (currentNumber < 0) {
-        currentNumberWrapper.style.color = 'yellow';
-    } else if (currentNumber == 0){
-        currentNumberWrapper.style.color = 'white';
-    }
+// a setInterval é um função do javascript que executa a função interna dela a uma
+// determinada quantidade de tempo (aqui está colocado para 100ms)
+// a função interna está como função anonima, poderia ser uma função com nome ou uma
+// arrow funcion 
+// Essa função anonima chama outra função a "operation", a ideia de chamar operation aqui
+// é evitar ficar repetindo código, já que a operation também será chamada pela função
+// clicked.
+function action(typeButton) {
+  interval = setInterval(function () {
+    operation(typeButton)
+  }, 100)
 }
+
+// para (interrompe) a contagem "matando" o setInteval
+// isso aconteceu qual o botão do mouse é "desclicado"
+function stop() {
+  clearInterval(interval)
+}
+
+// existe duas execuções, uma quando o botão do mouse é pressionado e seguro e outra
+// quando o botão do mouse é somente clicado, essa ultima é executado com a função abaixo
+function clicked(typeButton) {
+  operation(typeButton)
+}
+
